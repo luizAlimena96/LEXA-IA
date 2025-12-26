9fac2ebbe964844a650324c05ce951d0b75cf815
Rafael Borges Bezerra <leafartech@gmail.com>
2025-12-26 17:35:45 -0300

    GTM added to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5ZGPB3RT')
+        `}} />
+      </head>
       <body className="bg-gray-50 dark:bg-[#0a0a0f] transition-colors duration-300">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZGPB3RT"
+          height="0" width="0"></iframe></noscript>
         <Providers>
           <LayoutClient>{children}</LayoutClient>
         </Providers>
